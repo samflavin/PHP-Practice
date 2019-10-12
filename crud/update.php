8887d3c4bbec9ef3cd479601c6ab3e665c21553f
Sam Flavin <flavi022@d.umn.edu>
2019-10-12 13:27:40 -0500

    finished update.php

---
+++ crud/update.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+<?php include 'db.php';
+
+$sql = "select * from tasks";
+
+$rows=$db->query($sql);
+
+?>
+<html lang="en">
+<head>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/d3js/5.12.0/d3.min.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>To Do list</title>
+</head>
+<body>
+<div class="container">
+<h1 text-align="center">To Do List</h1>
+
+<div class="row" style="margin-top: 70px;">
+        <div class="col-md-10 col-md-offset-1">
+        <button type="button" data-target="#myModal" data-toggle="modal" class="btn btn-success">Update Task</button>
+        <button type="button" class="btn btn-default" text="right">Print</button>
+        <hr><br>
+
+        <form method="post" action="add.php" >
+            <div class="form-group">
+            <label>Task Name</label>
+            <input type="text" required name="task" class="form-control">
+            </div>
+            <input type="submit" name="send" value="Add Task" class="btn btn-success" >
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+      
+        </div>
+</div>
+</div>
+    
+</body>
+</html>
